87011893cac8f77df02aae9a8210d4a597e9864b
hkashyap0809 <hkashyap0809@gmail.com>
2024-03-04 22:32:02 +0530

    hello-world-api
    hello world api using RequestMapping and GetMapping

---
+++ restful-web-services/src/main/java/com/harshit/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.harshit.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	// / hello-world
+	//	@RequestMapping(method = RequestMethod.GET , path="/hello-world")
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
