8b7634c983b1389c7641eec0fb0ebf829cb1d957
hkashyap0809 <hkashyap0809@gmail.com>
2024-03-10 13:10:57 +0530

    generic-exception
    implementing generic exception handling for all resources

---
+++ restful-web-services/src/main/java/com/harshit/rest/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -0,0 +1,32 @@
+package com.harshit.rest.webservices.restfulwebservices.exception;
+
+import java.time.LocalDateTime;
+
+
+public class ErrorDetails {
+	
+	//timestamp - when the error occurred
+	//message
+	//details
+	
+	private LocalDateTime timestamp;
+	private String message;
+	private String details;
+	
+	public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+	
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public String getDetails() {
+		return details;
+	}
+}
