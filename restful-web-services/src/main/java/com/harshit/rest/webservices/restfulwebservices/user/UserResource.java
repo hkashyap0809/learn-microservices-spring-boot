bb0296ada9022c62bf08cfc5f206a899de3a638d
hkashyap0809 <hkashyap0809@gmail.com>
2024-03-12 12:10:48 +0530

    validation
    implementing validation for rest api

--- restful-web-services/src/main/java/com/harshit/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/harshit/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,10 +3,8 @@ package com.harshit.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UserResource {
@@ -43,7 +43,7 @@ public class UserResource {
 	}
 	
 	@PostMapping(path="/users")
-	public ResponseEntity<User> createuser(@RequestBody User user) {
+	public ResponseEntity<User> createuser(@Valid @RequestBody User user) {
 		User savedUser = userDaoService.save(user);
 		// /users/4 => /users/{id}  
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
