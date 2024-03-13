4500822edcfdcb6005e695e6f5c25d4108b51baf
hkashyap0809 <hkashyap0809@gmail.com>
2024-03-14 00:25:36 +0530

    initial-setup-tightly-coupled-game
    initial setup and creating a tightly coupled game

---
+++ learn-spring-framework/src/main/java/com/springframeworkdemo/learnspringframework/MarioGame.java
@@ -0,0 +1,20 @@
+package com.springframeworkdemo.learnspringframework;
+
+public class MarioGame {
+	
+	public void up() {
+		System.out.println("JUMP");
+	}
+	
+	public void down() {
+		System.out.println("DUCK");
+	}
+	
+	public void left() {
+		System.out.println("BACK");
+	}
+	
+	public void right() {
+		System.out.println("FRONT");
+	}
+}
